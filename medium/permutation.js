df8935f6159d147de94f7885155b064e6f4f03a5
liangpeter <liangpeter08@gmail.com>
2020-03-21 11:25:40 -0400

    permutation
    [QA: None]

---
+++ medium/permutation.js
@@ -0,0 +1,27 @@
+var permute = function(nums) {
+    const retVal = [];
+    recurse(nums, 0, retVal);
+    return retVal;
+};
+
+const recurse = function (nums, i, retVal) {
+    if (nums.length === i + 1) {
+        retVal.push(nums);
+    }
+    
+    for (let j = i; j < nums.length; j++) {
+        const swapped = swap(nums, i, j)
+        recurse(swapped, i + 1, retVal);
+    }
+}
+
+const swap = function (nums, i, j) {
+    if (i === j) {
+        return [...nums];
+    } else {
+        const dup = [...nums];
+        dup[i] = dup[j];
+        dup[j] = nums[i];
+        return dup;
+    }
+};
