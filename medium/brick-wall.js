0114f7ad42e252b5ba2f9ae88c5056a50cdce69a
liangpeter <liangpeter08@gmail.com>
2020-03-28 17:29:22 -0400

    add new questions

--- medium/brick-wall.js
+++ medium/brick-wall.js
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/brick-wall/
+
+var leastBricks = function(wall) {
+    const hashmap = {};
+    const total = wall.length;
+    for (const row of wall) {
+        let gap = 0;
+        for (let cell = 0; cell < row.length -1; ++cell) {
+            gap += row[cell];
+            hashmap[gap] = hashmap[gap] || 0;
+            hashmap[gap] += 1;
+        }
+    }
+    let largestGap = 0;
+    for (const k in hashmap) {
+        if (hashmap[k] > largestGap) {
+            largestGap = hashmap[k];
+        }
+    }
+    return total - largestGap;
+  };
--- medium/find-peak-element.js
+++ medium/find-peak-element.js
@@ -7,9 +7,9 @@ var findPeakElement = function(nums) {
     let j = nums.length - 1;
     while (i + 1 < j) {
         const mid = parseInt((i + j) / 2);
-        console.log(i + ' ' + j + '  ' + mid); 
+        // console.log(i + ' ' + j + '  ' + mid); 
         if (nums[i] <nums[mid] && nums[mid] > nums[j]){
-            console.log('hit');
+            // console.log('hit');
             return findMid(i, j, nums);
         } else if (nums[mid] > nums[i]) {
             i = mid;
@@ -25,7 +25,7 @@ var findPeakElement = function(nums) {
 };
 
 const findMid = (i, j, nums) => {
-    console.log(i, j);
+    // console.log(i, j);
     if (i + 1 >= j) {
         return null;
     }
@@ -34,9 +34,9 @@ const findMid = (i, j, nums) => {
         if (i + 1 === mid && j - 1 === mid) {
             return mid;
         } else {
-            console.log('left');
+            // console.log('left');
             const left = findMid(i, parseInt((mid + j) /2), nums);
-            console.log('right');
+            // console.log('right');
             const right = findMid(parseInt((i + 1 + mid)/ 2), j, nums);
             return left || right;
         }
